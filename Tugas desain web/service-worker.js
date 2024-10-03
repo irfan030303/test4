e85e0dbfbd8f12882516e60fb3d6dd8d1781e485
irfan030303 <146045333+irfan030303@users.noreply.github.com>
2024-10-04 04:02:24 +0700

    Add files via upload

---
+++ Tugas desain web/service-worker.js
@@ -0,0 +1,21 @@
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open('my-cache').then(cache => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/about/about.html',
+                '/contact/contact.html',
+                '/1.png'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
